2f1b7cb447ee2062683106e80e69eb7e3c90b700
cindirizkia22 <47911529+cindirizkia22@users.noreply.github.com>
2019-11-17 09:48:49 +0700

    Add files via upload

---
+++ categori-all.php
@@ -0,0 +1,18 @@
+<?php 
+include 'koneksi.php';
+$sql= 'select * from produk order by id desc ';
+$query = mysql_query($sql);
+echo '<div class="sub">
+<h1>SEMUA PRODUK</h1>';
+while ($row = mysql_fetch_array($query)){
+  echo '
+        <div class="produk">
+                <img src="'.$row['poto'].'" alt="batik modern">
+                <div class="nama" >'.$row['nama'].'</div>
+                <div class="harga">Rp.'.$row['harga'].'</div>
+         
+         </div>';
+}
+echo '</div>';
+?>
+
