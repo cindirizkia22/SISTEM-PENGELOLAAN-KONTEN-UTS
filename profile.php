04f29d72890cf2b234cdc5b94358932eed1660e2
cindirizkia22 <47911529+cindirizkia22@users.noreply.github.com>
2019-11-17 09:54:51 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>Cindi Batik</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrap">
+        <div class="header">
+            <img src="img/logo.png" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php">Product</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="akun.php">Akun</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+            <form action="search.php">
+                <input class="cari" type="text" name="car" placeholder="Cari...">
+            </form>
+        </div>
+        <div class="saya">
+        <img src="img/cindi.png" alt="cidiani">
+        <h2>Cindi Batik</h2>
+        <p>.Cindi Batik adalah toko online batik berbagai jenis dengan kualitas terbaik dan harga terjangkau, kami produsen batik yang dengan senang hati memberikan pelayanan 
+        kepada para customer yang terbaik. kami produsen batik yang selalu bersedia membantu Anda. cindi batik juga memberikan kualitas proruk terbaik yang akan awet
+        dan nyaman digunakan untuk acara resmi dan acara santai. menyediakan berbagai macam batik mulai dari
+        batik sederhana batik moderen batik pasangan batik pria dan wanita bahkan batik keluarga.</p>
+        </div>
+    </div>
+</body>
+
+</html>
