d0cf0c61eb5c45747725c88c9851058c3e9aedc6
cindirizkia22 <47911529+cindirizkia22@users.noreply.github.com>
2019-11-17 09:53:16 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>Cindi Batik</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrap">
+        <div class="header">
+            <img src="img/logo.png" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php">Product</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="akun.php">Akun</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+            <form action="search.php">
+                <input class="cari" type="text" name="car" placeholder="Cari...">
+            </form>
+        </div>
+         <div class="login">
+        <?php include 'formlogin.php'; ?>
+        </div>
+    </div>
+</body>
+
+</html>
